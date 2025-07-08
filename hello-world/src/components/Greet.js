5508e048af4f5ff2ec942e532c90fceaae3fe3c8
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2025-07-08 11:29:30 +0530

    class and functional components added

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Functional component
+
+// function Greet(){
+//     return <h1>Hello Priya</h1>
+// }
+
+// define it as ES6 arrow function
+const Greet = () => <h1>Hello Priya!</h1>
+
+export default Greet;
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+// Class component
+// try some changes
+class Welcome extends Component{
+    render(){
+       return <h1>Welcome Priya from Class component</h1>
+    }
+}
+
+export default Welcome
