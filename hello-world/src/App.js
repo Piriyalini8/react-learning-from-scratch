bf9252dcc12937bc24215c0ac272321af55b6e72
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2025-07-09 13:19:42 +0530

    v10- state in class components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Greet from './components/Greet';
 import Hello from './components/Hello';
+import Message from './components/Message';
 import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet name='Priya' heroName='Wonder Woman'>
+      <Message/>
+      {/* <Greet name='Priya' heroName='Wonder Woman'>
         <p>This is Children paragraph</p>
       </Greet>
       <Greet name='Kishan' heroName='SpiderMan'>
@@ -17,7 +19,7 @@ function App() {
         <p>This is Children paragraph</p>
       </Welcome>
       <Welcome name='Kishan' heroName='SpiderMan'/>
-      <Welcome name='Pavi' heroName='Bat man'/>
+      <Welcome name='Pavi' heroName='Bat man'/> */}
       {/* <Hello/> */}
     </div>
   );
