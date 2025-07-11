208783a95c495afc540ae0c501ca1f164bfa073a
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2025-07-11 13:06:39 +0530

    v13-event-handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import ClassClick from './components/ClassClick';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
 import Greet from './components/Greet';
 import Hello from './components/Hello';
 import Message from './components/Message';
@@ -8,6 +10,8 @@ import Welcome from './components/Welcome';
 function App() {
   return (
     <div className="App">
+      <ClassClick/>
+      <FunctionClick/>
       {/* <Counter/> */}
       {/* <Message/> */}
       {/* <Greet name='Priya' heroName='Wonder Woman'>
@@ -16,11 +20,11 @@ function App() {
       <Greet name='Kishan' heroName='SpiderMan'>
         <button>Action</button>
       </Greet> */}
-      <Greet name='Pavi' heroName='Bat man'/>
+      {/* <Greet name='Pavi' heroName='Bat man'/> */}
       {/* <Welcome name='Priya' heroName='Wonder Woman'>
         <p>This is Children paragraph</p>
       </Welcome> */}
-      <Welcome name='Kishan' heroName='SpiderMan'/>
+      {/* <Welcome name='Kishan' heroName='SpiderMan'/> */}
       {/* <Welcome name='Pavi' heroName='Bat man'/> */}
       {/* <Hello/> */}
     </div>
